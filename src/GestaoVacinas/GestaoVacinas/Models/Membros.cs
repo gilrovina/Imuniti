8818c35d3875686631f8eb86f6ea0132455f03a4
Alberto <alberto19967@gmail.com>
2024-10-22 11:35:32 -0300

    Migration

--- src/GestaoVacinas/GestaoVacinas/Models/Membros.cs
+++ src/GestaoVacinas/GestaoVacinas/Models/Membros.cs
@@ -1,7 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GestaoVacinas.Models
 {
+
+    [Table("Membros")]
     public class Membros
     {
         public int Id { get; set; }
