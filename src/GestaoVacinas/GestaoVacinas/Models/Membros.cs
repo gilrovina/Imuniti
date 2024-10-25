c5742e2a4e9f61f3b757ae236e54d2d77a8b7eb8
Alberto <alberto19967@gmail.com>
2024-10-25 19:30:37 -0300

    Correção de linha e estilo do membros cadastrados

--- src/GestaoVacinas/GestaoVacinas/Models/Membros.cs
+++ src/GestaoVacinas/GestaoVacinas/Models/Membros.cs
@@ -12,18 +12,22 @@ namespace GestaoVacinas.Models
         [Required(ErrorMessage = "O apelido é obrigatório.")]
         public required string Apelido { get; set; }
 
+        [Display(Name = "Nome Completo")]
         [Required(ErrorMessage = "O nome completo é obrigatório.")]
         public required string NomeCompleto { get; set; }
 
+        [Display(Name = "Data de Nascimento")]
         [Required(ErrorMessage = "A data de nascimento é obrigatória.")]
         [DataType(DataType.Date)]
 
         public required DateTime DataNascimento { get; set; }
 
+        [Display(Name = "CPF")]
         [Required(ErrorMessage = "O CPF é obrigatório.")]
         [StringLength(11, MinimumLength = 11, ErrorMessage = "O CPF deve ter 11 dígitos.")]
         public required string Cpf { get; set; }
 
+        [Display(Name = "Cartão Nacional de Saúde")]
         [StringLength(15, MinimumLength = 15, ErrorMessage = "O CNS deve ter 15 dígitos.")]
         public string? Cns { get; set; }
     }
